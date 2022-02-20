2aaeb1eecd1664f179b08dbb332b206f774d7476
PielordZZ <zzpieme@gmail.com>
2022-02-20 14:14:50 -0500

    completed queries 4-8 and did epilogue question

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -14,7 +14,36 @@ FROM dept_manager AS dm
 INNER JOIN department as dp ON dp.dept_id=dm.dept_id
 INNER JOIN employee as em ON dm.emp_id = em.emp_id;
 
+--4
+SELECT e.emp_id, e.last_name, e.first_name, d.dept_name
+FROM department AS d
+FULL JOIN dept_emp AS ed ON d.dept_id = ed.dept_id
+FULL JOIN employee AS e ON ed.emp_id = e.emp_id;
 
+--5
+SELECT first_name, last_name, sex
+FROM employee
+WHERE (first_name LIKE 'Hercules') AND (Left(last_name,1) = 'B');
+
+--6
+SELECT e.emp_id, e.last_name, e.first_name, d.dept_name
+FROM department AS d
+FULL JOIN dept_emp AS ed ON d.dept_id = ed.dept_id
+FULL JOIN employee AS e ON ed.emp_id = e.emp_id
+WHERE d.dept_name = 'Sales';
+
+--7
+SELECT e.emp_id, e.last_name, e.first_name, d.dept_name
+FROM department AS d
+FULL JOIN dept_emp AS ed ON d.dept_id = ed.dept_id
+FULL JOIN employee AS e ON ed.emp_id = e.emp_id
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+--8
+SELECT last_name, COUNT(last_name) AS num_of_family
+FROM employee
+GROUP BY last_name
+ORDER BY num_of_family DESC;
 
 
 
